2613f8d94b7f6439ced9b9656f876b7870f96dec
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-11 09:42:28 +0600

    Create FirstContract.sol

---
+++ FirstContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract FirstContract{
+    howareYoy
+    howAreYou
+}
+
+contract SecondContract{
+    how are you bro 
+    how are you bro 
+}
+
+contract ThirdContract{
+    yo yo honey singh
+    yo yo honey singh
+}
