29fbce362812179a486bc4d673774a3390904b9f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-11 09:59:43 +0600

    Create builtIn.sol

---
+++ builtIn.sol
@@ -0,0 +1,13 @@
+// SPDX- License-Indentifier : MIT 
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract firstContract{
+    msg.sender
+    msg.sig
+    msg.value
+
+
+    now
+}
