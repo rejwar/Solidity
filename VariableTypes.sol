564615442c0d834349c5122b5863d6f9b1c69ec5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-11 09:53:13 +0600

    Create VariableTypes.sol

---
+++ VariableTypes.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+
+contract FirstContract{
+    // variables types in solidity
+    // 1. Fixed size types
+
+    uint a;
+    bool isBool; true false
+    address metoyou;
+    bytes32 b;
+
+    // variables size types 
+    
+    string myName;
+    bytes b;
+    uint[] arr;
+    mapping ;
+
+    // 3. user define data types
+
+    struct car {
+        string brandName:
+        uint price;
+    }
+
+    enum Color{
+        Blue,
+        red,
+        White
+    }
+    color.Blue
+}
