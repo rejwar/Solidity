1ad422db5bbf6108a45eb36870e467cd637d6a52
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-13 03:45:38 +0600

    Create pure.sol

---
+++ pure.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// pure function - no change 
+
+contract myContract{
+
+function add(uint a , uint b ) public pure returns (uint)
+{
+    return a +b ;
+
+}
+}
