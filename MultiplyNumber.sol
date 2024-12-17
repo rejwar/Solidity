3e641f807d54030d1d01ffb1157b0b2725a3fc7f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-17 21:24:53 +0600

    Create MultiplyNumber.sol

---
+++ MultiplyNumber.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MultiplyNumber{
+    function multiply(uint x, uint y) public pure returns(uint){
+        return x* y;
+    }
+}
