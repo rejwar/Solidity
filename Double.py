45f0a483da9f666cd1e6129917bb6e373ca49195
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-17 20:50:21 +0600

    Create Double.py

---
+++ Double.py
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract doubleNumber{
+    function double(uint number) public pure returns(uint)
+    {
+        return number*2;
+    }
+}
