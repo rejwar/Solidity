d75bcc89cee07d3bb0576e0dad34e4dd40cc509d
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-17 22:38:10 +0600

    Create NewName.sol

---
+++ NewName.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.0;
+contract storeName{
+    string rifat;
+    function setName(string memory newName) public {
+        new = newName;
+    }
+function getName() public view returns (string memory){
+    return name;
+}
+
+}
