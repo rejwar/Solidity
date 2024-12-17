9a6270f93eab55259e274bf310a9fa4f421a7db7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-17 20:25:04 +0600

    Create SPDX.sol

---
+++ SPDX.sol
@@ -0,0 +1,9 @@
+// SPDX-license-Identifier : MIT
+pragma solidity ^0.8.18; // stating our version  // this is the solidity version
+contract AddNumbers{
+    //hasFavoriteNumber = false;
+    function add(uint256 a , uint256 b) public pure returns (uint256)
+    {
+        return a+b;
+    }
+}
