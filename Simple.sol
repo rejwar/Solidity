b5d858d4f1ab3f46b34719eebd6cd11ec7ca0e4c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-17 13:20:27 +0600

    Create Simple.sol

---
+++ Simple.sol
@@ -0,0 +1,12 @@
+// SPDX-license-Identifier : MIT
+pragma solidity ^0.8.18; // stating our version  // this is the solidity version
+
+
+contract SimpleStorage{
+    //hasFavoriteNumber = false;
+    uint256 favoriteNumber = 88;
+    bool hasFavoriteNumber = true ;
+    string favoriteNumberInText = "eight- eight";
+    address myAddress =bc1qxuah0lntd29jnhlue6px9w80vemar85yq70dkn; // 
+    bytes32 favoriteBytes32 = "cat"; // 
+}
