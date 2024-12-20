a3dc2283fb9443ffdc88348238d8c0445a7c2aff
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-20 18:20:16 +0600

    Create StructAbove.sol

---
+++ StructAbove.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./StructDeclaration";
+
+contract Todos {
+    Todo[] public todos;
+}
