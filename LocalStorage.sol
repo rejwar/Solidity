db793ca770180be105ff280335368037d3c46a39
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-21 18:52:14 +0600

    Create LocalStorage.sol

---
+++ LocalStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract local 
+{
+    function store() pure public returns(uint)
+{
+    string memory name = "ravi";
+    uint age =10;
+    return age;
+}
+}
