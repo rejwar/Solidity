6a2eb2fa64273dcd923931e6435c484ec64717f5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-21 22:27:37 +0600

    Create Negative.sol

---
+++ Negative.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage{
+    uint256 public  storeDataInVar = 1000;
+    uint8 public smallPositiveNum = 255;
+    int8 public smallNegativeveNum 127; 
+
+    string public player = 'Naymer';d
+}
