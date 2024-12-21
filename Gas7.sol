92f14d979b5b691e137e2615d4287d4b5d3f5ac5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-21 23:04:35 +0600

    Create Gas7.sol

---
+++ Gas7.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Example {
+    bytes4 public  myBytes = "Hi";
+    bytes3 public myFizedBytes = 0x616263;
+
+    bytes public myDynamicBytes = "Web Dev ";
+    uint256 public counter = 1;
+    address public sender;
+
+    function addByte() public {
+        myDynamicBytes = abi.encodePacked(myDynamicBytes ,"Lab");
+
+    }
+    function getByte() public view returns (string memory ){
+        return string(myDynamicBytes);
+    }
+
+    function incrementNumber() public {
+         counter = counter+1;
+    }
+
+    function getAddress() public view returns (address){
+        return address (this);
+    }
+
+}
