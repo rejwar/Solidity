cd66f24915c9dfe7d8cf78a05c5de4dd98608f24
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-22 04:07:25 +0600

    Create If7.sol

---
+++ If7.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract  helloWorld{
+    constructor() public {
+
+    }
+
+    function print() public pure returns (string)
+    {
+        return 'Hello world! First Simple Smart Contracts'; 
+    }
+}
