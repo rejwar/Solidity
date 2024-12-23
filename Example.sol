b978c928376e1b900c5ac3ef7da0be1da2a339f8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-23 17:21:25 +0600

    Create Example.sol

---
+++ Example.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+ 
+ contract Test{
+    function getResult() public view returns (uint product ,uint sum)
+    {
+        uint a = 1;
+        uint b = 2;
+        product = a*b;
+        sum = a+b;
+    }
+ }
