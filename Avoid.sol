d87215104e4a867166882318a4858b39dcc79826
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-23 15:28:44 +0600

    Create Avoid.sol

---
+++ Avoid.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract Coin{
+    struct Bank{
+        address owner;
+        uint balance;
+    }
+}
+
+if(x<3){
+    x +=1;
+} else if (x>7){
+    x-=1;
+} else{
+    x =5;
+}
+if (x < 3)
+x+=1;
+
+else 
+x -=1;
