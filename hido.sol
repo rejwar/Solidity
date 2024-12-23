6a174cff4118ce56c52037d4e2bcc43906a5887c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-23 20:55:43 +0600

    Create hido.sol

---
+++ hido.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Test{
+    uint public x;
+    function() external { x = 1;}
+}
+
+contract Sink{
+    function() external payable {}
+}
+
+contract Caller{
+    function callTest(Test test ) public returns(bool){
+        (bool success ,) =address(test).call(abi.encodingWithSignature("nonExistingFunction()"));
+        require (success);
+
+        address payable testPayable = address(uint160 (address(test)));
+
+        return (testPayable.send(2 ether));
+    }
+
+    function callSink(Sink sink) public returns(bool)
+    {
+        address payable sinkPayable = address(sink);
+        return (sinkPayable.send(2 ether))
+    }
+}
