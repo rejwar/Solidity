ca356f32e477836d969646ff2b3da6eec122400a
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-26 18:14:26 +0600

    Create LocalVariables.sol

---
+++ LocalVariables.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract demo{
+
+    uint c;
+    function local() public pure returns (uint){
+
+        uint a;
+        uint b;
+        return a+b;
+    }
+
+
+}
