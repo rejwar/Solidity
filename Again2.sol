2da75c35149bb45c5ee7d70ebf718cf37d1f22e5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-26 18:23:58 +0600

    Create Again2.sol

---
+++ Again2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract LocalVariableExample{
+    function CalculateSum() public pure returns(uint){
+        uint num1 = 10;
+        uint num2 = 20;
+        uint sum = num1 + num2;
+
+        return sum;
+    }
+}
