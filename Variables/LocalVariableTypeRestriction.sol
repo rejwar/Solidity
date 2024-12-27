dd81d9d5768629f682ae242ac81e94149b3508d0
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-27 18:22:30 +0600

    Create LocalVariableTypeRestriction.sol

---
+++ Variables/LocalVariableTypeRestriction.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TypeRestriction{
+    function CheckTypeRestriction() public pure returns (string memory){
+        string memory message = "Hello solidity ";
+        return message;
+    }
+}
