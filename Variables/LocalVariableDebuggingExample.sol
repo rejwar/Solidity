d024e96853214610e60d9cd992169a9eb29f4ca5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-27 18:39:21 +0600

    Create LocalVariableDebuggingExample.sol

---
+++ Variables/LocalVariableDebuggingExample.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DebuggingExample{
+    function DebugExample(uint a , uint b) public pure returns(uint){
+        uint result = a*b;
+        return result;
+    }
+}
