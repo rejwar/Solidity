78eb5b4ff4f9b29a7a5b9a4e118f360f37d3393c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-27 16:40:21 +0600

    Create CheckEvenOdd.sol

---
+++ Variables/CheckEvenOdd.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CheckEvenOdd{
+    function IsEven(uint number) public pure returns(bool){
+        bool IsEvenNumber = (number %2==0);
+        reutun IsEvenNumber;
+    }
+}
