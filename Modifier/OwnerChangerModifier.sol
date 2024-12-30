a65d4508e70d0191a80acb9434894a9c1ffbd375
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-30 14:28:30 +0600

    Create OwnerChangerModifier.sol

---
+++ Modifier/OwnerChangerModifier.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender ==owner, "You are not the owner!");
+        _;
+    }
+    function changeOwner(address newOwner) public onlyOwner{
+        owner = newOwner;
+    }
+}
