630961069b759b1a7b2c11d312549e72c4706b6a
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-01-23 10:44:43 +0600

    Update FirstERC20token.sol

--- ERC20Token/FirstERC20token.sol
+++ ERC20Token/FirstERC20token.sol
@@ -1,1 +1,23 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import 'https://github.com/rejwar/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract MyToken is ERC20{
+    address public admin;
+    constructor() ERC20 ('MyToken' , 'MTN'){
+        _mint(msg.sender, 10000 * 10 * 18);
+    }
+
+    function mint(address to , uint amount) external {
+        require(msg.sender == admin, 'Only admin');
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender , amount);
+    }
+}
+
