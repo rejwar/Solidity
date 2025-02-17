603cb31e5913a680f36ef9a31ea8379d582bf2c7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-02-17 21:01:36 +0600

    Update SimpleExternalCall.sol

--- External Call/SimpleExternalCall.sol
+++ External Call/SimpleExternalCall.sol
@@ -1,1 +1,14 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface TargetContract{
+    function TargetFunction( uint256 Value) external returns (bool);
+    }
+
+    contract Mycontract{
+        function CallTarget(address TargetAddress , uint256 Value) public  returns (bool){
+            TargetContract TC = TargetContract( TargetAddress);
+            return TC.TargetFunction(Value);
+        }
+    }
